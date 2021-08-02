75a1f1e98d26ce4603dd888cd9bed3f4638ed355
= <=>
2021-08-02 15:01:45 -0300

    Add the 'Balanced Brackets' exercise.

---
+++ balanced_brackets.py
@@ -0,0 +1,14 @@
+def check_balance(brackets):
+    balanced = 0
+    for bracket in brackets:
+        if bracket == '[':
+            balanced += 1
+        elif bracket == ']':
+            balanced -= 1
+            
+    return balanced == 0
+
+
+bracket_string = '[[[[]]]]'
+
+print(check_balance(bracket_string))
