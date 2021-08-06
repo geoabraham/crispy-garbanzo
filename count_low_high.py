2b59209137260af90df294c8c795f5e1caf7d2c1
Gerardo Abraham <geoabraham@tuta.io>
2021-08-06 16:12:57 -0300

    Add alternative solutions to count_low_high.py

--- count_low_high.py
+++ count_low_high.py
@@ -2,7 +2,7 @@
 # If these conditions are not met, the number is considered a low.
 # At the end of the function, you must return a list that contains the number of lows and highs, in that order.
 # In case the list is empty, you may return None.
-def count_low_high(num_list):
+def count_low_high_my_solution(num_list):
     if len(num_list) == 0:
         return None
     result = [0, 0]
@@ -12,3 +12,28 @@ def count_low_high(num_list):
         else:
             result[0] += 1
     return result
+
+
+def count_low_high_filter_lambda_solution(num_list):
+    if (len(num_list) == 0):
+        return None
+    high_list = list(filter(lambda n: n > 50 or n % 3 == 0, num_list))
+    low_list = list(filter(lambda n: n <= 50 and not n % 3 == 0, num_list))
+    return [len(low_list), len(high_list)]
+
+
+def count_low_high_list_comprehension_solution(num_list):
+    if (len(num_list) == 0):
+        return None
+    high_list = len([n for n in num_list if n > 50 or n % 3 == 0])
+    low_list = len([n for n in num_list if n <= 50 and not n % 3 == 0])
+    return [low_list, high_list]
+
+
+num_list = [20, 9, 51, 81, 50, 42, 77]
+
+print(count_low_high_my_solution(num_list))
+
+print(count_low_high_filter_lambda_solution(num_list))
+
+print(count_low_high_list_comprehension_solution(num_list))
