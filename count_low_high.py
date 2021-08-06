b607e35a0c3c69aae7b8549b2cf2d35d88465904
Gerardo Abraham <geoabraham@tuta.io>
2021-08-06 16:08:13 -0300

    Update count_low_high.py

--- count_low_high.py
+++ count_low_high.py
@@ -1,2 +1,14 @@
+# If a number is greater than 50 or divisible by 3, it will count as a high.
+# If these conditions are not met, the number is considered a low.
+# At the end of the function, you must return a list that contains the number of lows and highs, in that order.
+# In case the list is empty, you may return None.
 def count_low_high(num_list):
-    pass  # Replace with your code
+    if len(num_list) == 0:
+        return None
+    result = [0, 0]
+    for num in num_list:
+        if num > 50 or num % 3 == 0:
+            result[1] += 1
+        else:
+            result[0] += 1
+    return result
