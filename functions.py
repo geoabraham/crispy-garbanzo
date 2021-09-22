042fc8d07cde9f5917edc6b0f7d258bd7e5bb871
Gerardo Abraham <geoabraham@tuta.io>
2021-09-22 08:52:51 -0300

    add function arguments practice

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 file_name = "players.txt"
 
@@ -133,6 +134,79 @@ def roll_dice(num):
     return random.randint(1, num)
 
 
+def calc_hi_lo_avg(*args, log_to_console=False):
+    hi = int(bool(args)) and max(args)
+    lo = int(bool(args)) and min(args)
+    avg = (hi + lo) / 2
+    if log_to_console:
+        print(avg)
+    return avg
+
+
+def func(a, b=10):
+    print(a, b)
+
+
+def func(a, b, *args):
+    print(a, b, args)
+
+
+def func(a, b, *args, kw1, kw2=100):
+    print(a, b, args, kw1, kw2)
+
+
+def func(a, b=10, *, kw1, kw2=100):
+    print(a, b, kw1, kw2)
+
+
+def func(a, b, *args, kw1, kw2=100, **kwargs):
+    print(a, b, args, kw1, kw2, kwargs)
+
+
+def func(a, b=10, *, kw1, kw2=100, **kwargs):
+    print(a, b, kw1, kw2, kwargs)
+
+
+def func(*args):
+    print(args)
+
+
+def func(**kwargs):
+    print(kwargs)
+
+
+def func(*args, **kwargs):
+    print(args, kwargs)
+
+
+def time_it(fn, *args, rep=5, **kwargs):
+    start = time.perf_counter()
+
+    for i in range(rep):
+        fn(*args, **kwargs)
+
+    end = time.perf_counter()
+    return (end - start) / rep
+
+
+def compute_powers_1(n, *, start=1, end):
+    # using a for loop
+    results = []
+    for i in range(start, end):
+        results.append(n**i)
+    return results
+
+
+def compute_powers_2(n, *, start=1, end):
+    # using a list comprehension
+    return [n**i for i in range(start, end)]
+
+
+def compute_powers_3(n, *, start=1, end):
+    # using a generator expression
+    return (n**i for i in range(start, end))
+
+
 # calc()
 # guess_secret_word()
 # print (raise_to_power(2, 3))
@@ -140,8 +214,25 @@ def roll_dice(num):
 
 # print(replace_vowel_for_number("Eggs Please !!"))
 
-append_file()
-read_file()
-print("FLUSH!")
-write_file()
-read_file()
+# help(func)
+
+func(10, 12, 13, d=30)
+
+# compute_powers_1(2, end=5)
+
+# compute_powers_2(2, end=5)
+
+# list(compute_powers_3(2, end=5))
+
+time_it(compute_powers_1, n=2, end=20000, rep=4)
+
+time_it(compute_powers_2, 2, end=20000, rep=4)
+
+time_it(compute_powers_3, 2, end=20000, rep=4)
+
+
+# append_file()
+# read_file()
+# print("FLUSH!")
+# write_file()
+# read_file()
