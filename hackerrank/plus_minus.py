aade1e88787b46086969e9610f31c94d658e7ba9
Gerardo Abraham <geoabraham@tuta.io>
2023-03-31 14:46:48 -0300

    chore: update names

--- hackerrank/plus_minus.py
+++ hackerrank/plus_minus.py
@@ -13,11 +13,11 @@ import sys
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 
-def plusMinus(arr):
+def plus_minus(array):
     # Write your code here
-    positive_ratio = len([num for num in arr if num > 0]) / len(arr)
-    negative_ratio = len([num for num in arr if num < 0]) / len(arr)
-    zero_ratio = len([num for num in arr if num == 0]) / len(arr)
+    positive_ratio = len([num for num in array if num > 0]) / len(array)
+    negative_ratio = len([num for num in array if num < 0]) / len(array)
+    zero_ratio = len([num for num in array if num == 0]) / len(array)
     print("{:.6f}".format(positive_ratio))
     print("{:.6f}".format(negative_ratio))
     print("{:.6f}".format(zero_ratio))
@@ -25,7 +25,5 @@ def plusMinus(arr):
 
 if __name__ == '__main__':
     n = 6  # int(input().strip())
-
     arr = [-4, 3, -9, 0, 4, 1]  # list(map(int, input().rstrip().split()))
-
-    plusMinus(arr)
+    plus_minus(arr)
