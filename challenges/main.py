143b78b56f9b4522149529c536e8563d2f6cfe5a
Gerardo Abraham <geoabraham@tuta.io>
2023-04-18 09:17:50 -0300

    fix: add main function

--- challenges/main.py
+++ challenges/main.py
@@ -2,9 +2,12 @@ import challenge1
 import challenge2
 import challenge3
 
-challenge1.main()
-challenge2.main()
-challenge3.main()
+
+def main():
+    challenge1.main()
+    challenge2.main()
+    challenge3.main()
+
 
 if __name__ == "__main__":
     main()
