61b6ed7ed5f2a8b97fadc963bd8b6d36c97fca0d
Gerardo Abraham <18366934+geoabraham@users.noreply.github.com>
2024-12-19 01:06:18 -0300

    feat: :sparkles: improve caesar cypher

--- cracking_codes/caesar_cipher.py
+++ cracking_codes/caesar_cipher.py
@@ -1,41 +1,44 @@
-LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-NUMBERS = '1234567890'
-SPACE_AND_SPECIAL_CHARACTERS = ' ¡!¿?.+*/@#$%&=()-_:;,ñÑáéíóúÁÉÍÓÚ'
-SYMBOLS = LETTERS + NUMBERS + SPACE_AND_SPECIAL_CHARACTERS
+LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+NUMBERS = "1234567890"
+SPACE_AND_SPECIAL_CHARATERS = " ¡!¿?.+*/@#$%&=()-_:;,ñÑáéíóúÁÉÍÓÚ"
+SYMBOLS = LETTERS + NUMBERS + SPACE_AND_SPECIAL_CHARATERS
 
 
-def main():
-    secret_message = 'Súper Secret Message!!'
+def main() -> None:
+    """_summary_"""
+    secret_message = "Mensaje súper secretooo!!! Iñaki."
     key = 13
 
     encrypted_message = encrypt_message(secret_message, key)
+    assert encrypted_message != secret_message
 
-    print(encrypted_message)
-
-    decrypted_message = encrypt_message(encrypted_message, key, 'decrypt')
-
-    print(decrypted_message)
+    decrypted_message = encrypt_message(encrypted_message, key, mode=False)
+    assert decrypted_message == secret_message
 
 
-def encrypt_message(message, key, mode='encrypt'):
-    encrypted_message = ''
+def encrypt_message(message: str, key: int, mode: bool = True) -> str:
+    """
+    Encrypts or decrypts a message using Caesar cipher.
 
+    Args:
+        message (str): The message to be encrypted/decrypted.
+        key (int): The number of positions by which letters should be shifted.
+        mode (bool, optional): If True, the function will encrypt the message.
+            If False, it will decrypt the message. Defaults to True.
+
+    Returns:
+        str: The encrypted/decrypted message.
+    """
+    encrypted_message = ""
     for symbol in message:
         if symbol in SYMBOLS:
             symbol_index = SYMBOLS.find(symbol)
-            encrypted_index = 0
 
             # Perform encryption/decryption:
-            if mode == 'encrypt':
-                encrypted_index = symbol_index + key
-            elif mode == 'decrypt':
-                encrypted_index = symbol_index - key
-
-            # Handle wrap-around, if needed:
-            if encrypted_index >= len(SYMBOLS):
-                encrypted_index -= len(SYMBOLS)
-            elif encrypted_index < 0:
-                encrypted_index += len(SYMBOLS)
+            if mode:
+                encrypted_index = (symbol_index + key) % len(SYMBOLS)
+            else:
+                encrypted_index = (symbol_index - key) % len(SYMBOLS)
 
             encrypted_message += SYMBOLS[encrypted_index]
         else:
@@ -44,5 +47,5 @@ def encrypt_message(message, key, mode='encrypt'):
     return encrypted_message
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
