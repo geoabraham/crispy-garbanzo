f5efdc6f68ed2734f463de5e635853e1cf24e7a0
Gerardo Abraham <18366934+geoabraham@users.noreply.github.com>
2024-12-19 02:51:28 -0300

    style: :lipstick: apply black formatter

--- cracking_codes/transposition_columnar_cipher_test.py
+++ cracking_codes/transposition_columnar_cipher_test.py
@@ -6,12 +6,12 @@ random.seed(42)  # set the random "seed" to a static value
 for i in range(20):  # run 20 tests
     # Generate random messages to test.
     # The message will have a random length:
-    message = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' * random.randint(4, 40)
+    message = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" * random.randint(4, 40)
 
     # Convert the message string to a list to shuffle it.
     message = list(message)
     random.shuffle(message)
-    message = ''.join(message)  # convert list to string
+    message = "".join(message)  # convert list to string
 
     print('Test #%s: "%s..."' % (i + 1, message[:50]))
 
@@ -23,8 +23,8 @@ for i in range(20):  # run 20 tests
         # If the decryption doesn't match the original message,
         # display an error message and quit.
         if message != decrypted:
-            print('Mismatch with key %s and message %s.' % (key, message))
-            print('Decrypted as: ' + decrypted)
+            print("Mismatch with key %s and message %s." % (key, message))
+            print("Decrypted as: " + decrypted)
             sys.exit()
 
-print('Transposition cipher test passed.')
+print("Transposition cipher test passed.")
