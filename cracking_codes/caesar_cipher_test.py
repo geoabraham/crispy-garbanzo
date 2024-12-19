b9fbae9511d107d98632919bfba4eb2c69893d79
Gerardo Abraham <18366934+geoabraham@users.noreply.github.com>
2024-12-19 01:28:08 -0300

    feat: :sparkles: implement encrypt mode to `bool`

--- cracking_codes/caesar_cipher_test.py
+++ cracking_codes/caesar_cipher_test.py
@@ -1,5 +1,4 @@
 import random
-import sys
 
 import caesar_cipher
 
@@ -7,25 +6,25 @@ random.seed(42)  # set the random "seed" to a static value
 for i in range(20):  # run 20 tests
     # Generate random messages to test.
     # The message will have a random length:
-    message = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' * random.randint(4, 40)
+    message = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" * random.randint(4, 40)
 
     # Convert the message string to a list to shuffle it.
     message = list(message)
     random.shuffle(message)
-    message = ''.join(message)  # convert list to string
+    message = "".join(message)  # convert list to string
 
     print('Test #%s: "%s..."' % (i + 1, message[:50]))
 
     # Check all possible keys for each message.
     for key in range(1, 26):
         encrypted = caesar_cipher.encrypt_message(message, key)
-        decrypted = caesar_cipher.encrypt_message(encrypted, key, 'decrypt')
+        decrypted = caesar_cipher.encrypt_message(encrypted, key, False)
 
         # If the decryption doesn't match the original message,
         # display an error message and quit.
         if message != decrypted:
-            print('Mismatch with key %s and message %s.' % (key, message))
-            print('Decrypted as: ' + decrypted)
+            print("Mismatch with key %s and message %s." % (key, message))
+            print("Decrypted as: " + decrypted)
             sys.exit()
 
 print("Caesar's cipher test passed.")
