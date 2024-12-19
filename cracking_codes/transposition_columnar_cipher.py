3fa19d7fdbb34e9d0bbef4048e46aee796c9ac5f
Gerardo Abraham <18366934+geoabraham@users.noreply.github.com>
2024-12-19 02:21:03 -0300

    refactor: :recycle: improve columnar transposition cipher

--- cracking_codes/transposition_columnar_cipher.py
+++ cracking_codes/transposition_columnar_cipher.py
@@ -1,55 +1,46 @@
-import math as m
+import math
 
 
 def main():
     secret_message = "Súper Secret Message!!"
     key = 8
 
-    secret_message_encrypted = encrypt_message(secret_message, key)
+    encrypted_message = encrypt_message(secret_message, key)
+    assert encrypted_message != secret_message
 
-    print(secret_message_encrypted + '|')
-
-    secret_message_decrypted = decrypt_message(secret_message_encrypted, key)
-
-    print(secret_message_decrypted + '|')
-
-    return
+    decrypted_message = decrypt_message(encrypted_message, key)
+    assert decrypted_message == secret_message
 
 
-def encrypt_message(message, key):
-    # If you imagine each column is a string,
-    # the result would be a list of eight strings
-    encrypted_message = [''] * key
-
+def encrypt_message(message: str, key: int) -> str:
+    """Encrypts a given message using columnar transposition cipher."""
+    encrypted_message = [""] * key
     for column in range(key):
         index = column
         while index < len(message):
-            # Place the character at index in the string in column.
             encrypted_message[column] += message[index]
-            # move index over
             index += key
 
-    return ''.join(encrypted_message)
+    return "".join(encrypted_message)
 
 
-def decrypt_message(message, key):
-    columns = int(m.ceil(len(message) / float(key)))
-    shaded_boxes = (columns * key) - len(message)
-    decrypted_message = [''] * columns
+def decrypt_message(encrypted_message: str, key: int) -> str:
+    """Decrypts a given encrypted message using columnar transposition cipher."""
+    columns = math.ceil(len(encrypted_message) / float(key))
+    shaded_boxes = (columns * key) - len(encrypted_message)
+    decrypted_message = [""] * columns
 
     column = 0
     row = 0
-    for symbol in message:
+    for symbol in encrypted_message:
         decrypted_message[column] += symbol
         column += 1
-        if (column == columns) or (column == columns - 1 and row >= key - shaded_boxes):
+        if column == columns or (column == columns - 1 and row >= key - shaded_boxes):
             column = 0
             row += 1
 
-    return ''.join(decrypted_message)
+    return "".join(decrypted_message)
 
 
-# If transposition_columnar_cipher.py is run (instead of imported as a module)
-# call the main() function.
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
